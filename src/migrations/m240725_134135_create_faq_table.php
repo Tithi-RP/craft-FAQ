7ecdfef4985fe1b35c3513130d22903f2b4e2f14
tithi_rp <tithi92.rp@gmail.com>
2024-08-30 12:16:12 +0530

    plugin-dev

---
+++ src/migrations/m240725_134135_create_faq_table.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace craft\contentmigrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m240725_134135_create_faq_table migration.
+ */
+class m240725_134135_create_faq_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+
+        public function safeUp()
+        {
+            $this->createTable('{{%faqmanager_faqs}}', [
+                'id' => $this->primaryKey(),
+                'question' => $this->text()->notNull(),
+                'answer' => $this->text()->notNull(),
+                'type' => $this->string(255)->notNull(),
+                'dateCreated' => $this->dateTime()->notNull(),
+                'dateUpdated' => $this->dateTime()->notNull(),
+                'uid' => $this->uid()->notNull(),
+            ]);
+        }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown(): bool
+    {
+        echo "m240725_134135_create_faq_table cannot be reverted.\n";
+        return false;
+    }
+}
